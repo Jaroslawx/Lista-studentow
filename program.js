c64e3ee2e1a094e162a6f0f86e896cd664efc94f
JaroslawX <ad.jaro@wp.pl>
2023-04-04 12:48:48 +0200

    Added new methods

--- program.js
+++ program.js
@@ -38,14 +38,26 @@ class ListaStudentow {
         this.nextIndexNumber++; // zwiększenie wartości dla następnego numeru indeksu
         return student;
     }
+
+    removeStudentByIndex(index) {
+        const indexToRemove = this.students.findIndex(student => student.indexNumber === index);
+        if (indexToRemove === -1) {
+            return false; // jeśli nie znaleziono studenta z takim numerem indeksu, to zwraca false
+        }
+        this.students.splice(indexToRemove, 1); // usuwa studenta z listy
+        return true; // zwraca true, jeśli udało się usunąć studenta
+    }
 }
 
+// Testowanie
 const lista = new ListaStudentow([
     new Student("Adam", "Kowalski", 1, 75),
     new Student("Anna", "Nowak", 2, 82),
-    new Student("Piotr", "Wisniewski", 3, 90),]);
+    new Student("Piotr", "Wisniewski", 3, 90),
+]);
   
 lista.addStudent("Maria", "Szymanska", 60);
+lista.removeStudentByIndex(2);
 
 for (let i = 0; i < lista.students.length; i++) {
     console.log(lista.students[i].getData());
